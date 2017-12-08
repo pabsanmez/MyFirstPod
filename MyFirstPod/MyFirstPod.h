5a49f73a3ca5f8781c4563f9240a38c10978d89d
pabsanmez <pabsanmez@gmail.com>
2017-12-08 20:20:20 +0100

    Initial Commit

---
+++ MyFirstPod/MyFirstPod.h
@@ -0,0 +1,19 @@
+//
+//  MyFirstPod.h
+//  MyFirstPod
+//
+//  Created by Pablo Sánchez Gómez on 8/12/17.
+//  Copyright © 2017 Pablo Sánchez Gómez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyFirstPod.
+FOUNDATION_EXPORT double MyFirstPodVersionNumber;
+
+//! Project version string for MyFirstPod.
+FOUNDATION_EXPORT const unsigned char MyFirstPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFirstPod/PublicHeader.h>
+
+
